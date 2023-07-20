3f4bc6bc4d87917079f0cd743c662d9f6eb14403
Eddie Shin <eshinhw@gmail.com>
2023-07-19 23:22:36 -0400

    updated db connection for download functionality

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
+from sqlalchemy import create_engine
 
-df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv")
+
+CONN_STRING = "postgresql://postgres:postgresql123@localhost:5432/stocks"
+ENGINE = create_engine(CONN_STRING)
 
 app = Dash(__name__)
 
@@ -16,26 +19,9 @@ app.layout = html.Div(
                     [
                         html.Li(children="United States (NYSE, NASDAQ, Amex)"),
                         html.Button("CSV", id="us-csv-btn", style={"cursor": "pointer"}),
-                        html.Button("JSON", id="us-json-btn"),
-                    ],
-                    style={
-                        "display": "flex",
-                        "gap": "10px",
-                        "justify-content": "start",
-                        "align-items": "center",
-                        "margin-left": "30px",
-                    },
-                )
-            ],
-            style={},
-        ),
-        html.Div(
-            [
-                html.Div(
-                    [
-                        html.Li(children="United States (NYSE, NASDAQ, Amex)"),
-                        html.Button("CSV", id="us-csv-btn", style={"cursor": "pointer"}),
-                        html.Button("JSON", id="us-json-btn"),
+                        dcc.Download(id="download-df-csv"),
+                        html.Button("Excel", id="us-excel-btn"),
+                        dcc.Download(id="download-df-excel"),
                     ],
                     style={
                         "display": "flex",
@@ -51,6 +37,18 @@ app.layout = html.Div(
         html.H2(children="Financial News Headlines"),
     ]
 )
+
+
+@callback(Output("download-df-csv", "data"), Input("us-csv-btn", "n_clicks"), prevent_initial_call=True)
+def download_csv(n_clicks):
+    df = pd.read_sql("SELECT * FROM us_tickers_kr", ENGINE.connect())
+    return dcc.send_data_frame(df.to_csv, "us_tickers_kr.csv")
+
+
+@callback(Output("download-df-excel", "data"), Input("us-excel-btn", "n_clicks"), prevent_initial_call=True)
+def download_excel(n_clicks):
+    df = pd.read_sql("SELECT * FROM us_tickers_kr", ENGINE.connect())
+    return dcc.send_data_frame(df.to_excel, "us_tickers_kr.xlsx", sheet_name="Sheet_name_1")
 
 
 if __name__ == "__main__":
